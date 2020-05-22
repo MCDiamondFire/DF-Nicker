8027f790fb110f03931ed78b8fdf040cdb8bf253
Owen1212055 <owen1212055@gmail.com>
2020-05-21 21:20:24 -0400

    Java rewrite

--- src/events/guildDelete.ts
+++
@@ -1,8 +0,0 @@
-import * as Discord from "discord.js";
-import { MongoClient } from "../database/client";
-
-var coll = MongoClient.db("DF-Nicker").collection("GuildConfig");
-
-module.exports = (guild: Discord.Guild) => {
-  coll.findOneAndDelete({ guildId: guild.id });
-};
---
+++ src/main/java/com/owen1212055/dfnicker/DFNicker.java
@@ -0,0 +1,33 @@
+package com.owen1212055.dfnicker;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
+
+public class DFNicker {
+    public static JDA jda;
+
+    // Please note, I have plans to rewrite this in the future.
+
+    public static void main(String[] args) throws LoginException {
+        ArrayList<GatewayIntent> intents = new ArrayList<>();
+        intents.add(GatewayIntent.GUILD_PRESENCES);
+        intents.add(GatewayIntent.GUILD_MEMBERS);
+
+        JDABuilder builder = JDABuilder.create(SensitiveData.TOKEN, intents);
+
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.addEventListeners(new Events());
+
+        builder.disableCache(CacheFlag.EMOTE, CacheFlag.VOICE_STATE, CacheFlag.MEMBER_OVERRIDES, CacheFlag.ACTIVITY, CacheFlag.CLIENT_STATUS);
+
+        jda = builder.build();
+
+
+    }
+}
